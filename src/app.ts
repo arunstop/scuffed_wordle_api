893dbe335644c01ca5bba43df5018769231fad65
arunstop <arunstop@gmail.com>
2022-02-01 04:15:20 +0700

    ADDED cors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import createError, { HttpError } from 'http-errors';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
