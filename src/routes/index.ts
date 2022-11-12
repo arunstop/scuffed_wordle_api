7c72e9f8c4c04a79973ca1829f47253154d7de43
arunstop <arunstop@gmail.com>
2022-11-12 22:56:54 +0700

    fixed error when rendering index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Application, Request, Response, Router } from "express";
 module.exports = Router().get(
     "/",
     async (req: Request, res: Response): Promise<Response> => {
-        res.render('index', { title: 'Express' });
+        // res.render('index', { title: 'Express' });
         return res.status(200).send({
             message: `Hello Worlds! ${__dirname}`,
         });
